03ebf3ec102af308ca0c1d4b6274e5869b99930b
Marcus Edralin <medralin36@yahoo.com>
2021-04-29 12:09:55 -0700

    OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '203527601358461',
+        clientSecret: '1a5ee823b22f695c87d1db1cedb26989'
+    }
 }
